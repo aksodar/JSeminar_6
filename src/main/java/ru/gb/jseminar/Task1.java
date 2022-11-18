f0a664012bb36fd4c66b5bf7ac627c6bbb6b45d2
Inna Osipova <rutheva_inna@mail.ru>
2022-11-18 15:12:08 +0300

    % уникальных данных

--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,15 +1,28 @@
 package ru.gb.jseminar;
 
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Task1 {
 
     public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        int [] arr = task1.initArray(1000);
+        System.out.println(task1.getPercentUniqueValues(arr));
 
     }
 
     // Напишите метод, который заполнит массив из 1000 элементов случайными цифрами от 0 до 24.
-    public int[] initArray(){
+    public int[] initArray(int arraySise){
+        Random random = new Random();
+        int [] myArray = new int [arraySise];
+        for (int i = 0; i < arraySise; i++) {
+            myArray [i] = random.nextInt(10);
+        }
 
-        return new int[]{};
+        return myArray;
     }
 
     // Создайте метод, в который передайте заполненный выше массив и с помощью Set вычислите процент уникальных
@@ -17,7 +30,13 @@ public class Task1 {
     // Для вычисления процента используйте формулу:
     // процент уникальных чисел = количество уникальных чисел * 100 / общее количество чисел в массиве.
     public double getPercentUniqueValues(int[] array){
+        Set <Integer> mySet = new HashSet<>();
+        for (Integer i : array) {
+            mySet.add(i);
+        }
+        
+        double result = mySet.size() *100/ array.length;
 
-        return 0.0d;
+        return result;
     }
 }
