5967bfd06a41edca122036c973ca574c5119a76e
Taisiya <lebei@mail.ru>
2022-11-20 14:52:46 +0300

    Homework done

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -2,8 +2,10 @@ package ru.gb.jseminar;
 
 import ru.gb.jseminar.data.Notebook;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Homework {
 
@@ -14,22 +16,50 @@ public class Homework {
     //      Пример: 1 - ОЗУ, 2 - Объем ЖД, 3 - Операционная система, 4 - Цвет
     // 4) Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
     public static void main(String[] args) {
-
-    }
-
-    public Notebook createNotebook(){
-
-        return null;
+        Homework hw = new Homework();
+        List<Notebook> products = hw.initListNotebooks();
+        Map<String, String> filterParam = new TreeMap<>();
+        filterParam.put("color", "black");
+        filterParam.put("model", "dell");
+        List<Notebook> FiltredProducts = hw.filter(filterParam, products);
+        hw.printList(FiltredProducts);
+        
     }
 
     public List<Notebook> initListNotebooks(){
-
-        return null;
+        List<Notebook> product = new ArrayList<>();
+        Notebook item_1 = new Notebook("red","hp","8");
+        Notebook item_2 = new Notebook("blue","acer","16");
+        Notebook item_3 = new Notebook("black","dell","32");
+        product.add(item_1);
+        product.add(item_2);
+        product.add(item_3);
+        return product;
     }
 
-    public List<Notebook> filter(Map<String, String> params){
+    public List<Notebook> filter(Map<String, String> params, List<Notebook> prod){
+        List<Notebook> filtredList = new ArrayList<>();
 
-        return null;
+        for (Notebook item: prod) {
+            List<String> tmp = new ArrayList<>();
+
+            for (String param: params.keySet()) {
+
+                if (item.CreateDict().get(param).equals(params.get(param))){
+                    tmp.add("ok");
+                }
+            }
+            if (tmp.size() == params.size()) {
+                filtredList.add(item);
+            }
+
+        }
+
+        return filtredList;
     }
-
+    public void printList(List<Notebook> prod){
+        for (Notebook item: prod) {
+            System.out.println(item.CreateDict());
+        }
+}
 }
