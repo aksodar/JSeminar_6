bcc9b96a66bf304da4a85ad2b0cba2d5afdbba29
LeaClover <plesen_14@mail.ru>
2022-11-21 20:25:41 +0300

    edit project data notebook

--- src/main/java/ru/gb/jseminar/data/Notebook.java
+++ src/main/java/ru/gb/jseminar/data/Notebook.java
@@ -1,4 +1,35 @@
 package ru.gb.jseminar.data;
 
 public class Notebook {
+
+    private int id;
+    private String name;
+    private String model;
+    private String typeOS;
+    private int valueRAM;
+    private String color;
+    private String typeMemory;
+    private String valueOfMemory;
+    private double costRUB;
+    private boolean gameType;
+
+    public Notebook(int id, String name, String model, String typeOS, int valueRAM, 
+            String color, String typeMemory, String valueOfMemory, double costRUB, boolean gameType) {
+        this.id = id;      
+        this.name = name;
+        this.model = model;
+        this.typeOS = typeOS; 
+        this.valueRAM = valueRAM;
+        this.color = color;
+        this.typeMemory = typeMemory;
+        this.valueOfMemory = valueOfMemory;
+        this.costRUB = costRUB;
+        this.gameType = gameType;
+    }
+
+    @Override
+		public String toString() {
+				return String.format("id-%d %s %s (OS:%s, RAM:%d GB, color:%s, %s:%s, price:%.2f RUB, gameType:%s)\n", id, name, model, typeOS, valueRAM, color, typeMemory, valueOfMemory, costRUB, gameType);
+		}
+    
 }
