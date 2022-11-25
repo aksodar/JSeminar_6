2835f628db156e688e00db945ff78813c1bd8ce5
Nikita <smiels@vk.com>
2022-11-25 16:35:14 +0300

    add hw+test

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,26 +1,34 @@
 package ru.gb.jseminar;
 
+import java.util.*;
+
 public class Task0 {
 
     public static void main(String[] args) {
-
+        Task0 t0 = new Task0();
+        t0.initHashSet();
+        t0.initLinkedHashSet();
+        t0.initTreeSet();
     }
 
     // Создайте HashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
     // Распечатайте содержимое данного множества.
     public void initHashSet(){
-
+        Set<Integer> hashset = new HashSet<>(Arrays.asList(1, 2, 3, 2, 4, 5, 6, 3));
+        System.out.println(hashset);
     }
 
     // Создайте LinkedHashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
     // Распечатайте содержимое данного множества.
     public void initLinkedHashSet(){
-
+        Set<Integer> linkedhashset = new LinkedHashSet<>(Arrays.asList(1, 2, 3, 2, 4, 5, 6, 3));
+        System.out.println(linkedhashset);
     }
 
     // Создайте TreeSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
     // Распечатайте содержимое данного множества.
     public void initTreeSet(){
-
+        Set<Integer> treeset = new TreeSet<>(Arrays.asList(1, 2, 3, 2, 4, 5, 6, 3));
+        System.out.println(treeset);
     }
 }
--- src/test/java/ru/gb/jseminar/HomeworkTest.java
+++ src/test/java/ru/gb/jseminar/HomeworkTest.java
@@ -1,5 +1,58 @@
 package ru.gb.jseminar;
 
-class HomeworkTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.gb.jseminar.data.Notebook;
 
+import java.util.*;
+
+class HomeworkTest {
+    @Test
+    void filterSuccessful() {
+        Homework hwTest = new Homework();
+        List<Notebook> notebooks = new ArrayList<>();
+        notebooks.add(new Notebook("apple", 16, 'M', "UHD Graphics 630", 150000));
+        notebooks.add(new Notebook("acer", 8, 'n', "gtx1050ti", 80000));
+        Map<String, String> params = new HashMap<>();
+        params.put("os", "M");
+
+        List<Notebook> expected = new ArrayList<>();
+        expected.add(new Notebook("apple", 16, 'M', "UHD Graphics 630", 150000));
+
+        List<Notebook> actual = hwTest.filter(params, notebooks);
+
+        Assertions.assertEquals(expected.toString(), actual.toString());
+
+    }
+
+    @Test
+    void filterParamsEmpty(){
+        Homework hwTest = new Homework();
+        List<Notebook> notebooks = new ArrayList<>();
+        notebooks.add(new Notebook("apple", 16, 'M', "UHD Graphics 630", 150000));
+        notebooks.add(new Notebook("acer", 8, 'n', "gtx1050ti", 80000));
+        Map<String, String> params = new HashMap<>();
+
+        List<Notebook> expected = new ArrayList<>();
+        expected.add(new Notebook("apple", 16, 'M', "UHD Graphics 630", 150000));
+        expected.add(new Notebook("acer", 8, 'n', "gtx1050ti", 80000));
+
+        List<Notebook> actual = hwTest.filter(params, notebooks);
+
+        Assertions.assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    void filterListEmpty(){
+        Homework hwTest = new Homework();
+        List<Notebook> notebooks = new ArrayList<>();
+
+        Map<String, String> params = new HashMap<>();
+        params.put("os", "M");
+
+        List<Notebook> actual = hwTest.filter(params, notebooks);
+
+        // а вот тут мне уже не хватило соображалки, чтобы прокинуть исключение в тест
+        // понял, что это делается через Assert.throw, но все попытки "подобрать" нужный код не увенчались успехом
+    }
 }
