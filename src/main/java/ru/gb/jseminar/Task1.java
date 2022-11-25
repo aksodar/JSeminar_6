314f5efc4ecf2a080bdb0d28e0446673447ce78f
Aleksey <rgd666@yandex.ru>
2022-11-25 21:39:45 +0300

    Task

--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,23 +1,39 @@
 package ru.gb.jseminar;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Task1 {
 
     public static void main(String[] args) {
 
+        Task1 tsk = new Task1();
+        int[] array = tsk.initArray(1000);
+        System.out.println(tsk.getPercentUniqueValues(array));
     }
 
     // Напишите метод, который заполнит массив из 1000 элементов случайными цифрами от 0 до 24.
-    public int[] initArray(){
+        public int[] initArray(int arraySize){
+            int[] myArr = new int[arraySize];
+            Random rnd = new Random();
+            for (int i = 0; i < arraySize; i++){
+                myArr[i] = rnd.nextInt(10);
+            }
+            return myArr;
+        }
 
-        return new int[]{};
-    }
+        // Создайте метод, в который передайте заполненный выше массив и с помощью Set вычислите процент уникальных
+        // значений в данном массиве и верните его в виде числа с плавающей запятой.
+        // Для вычисления процента используйте формулу:
+        // процент уникальных чисел = количество уникальных чисел * 100 / общее количество чисел в массиве.
+        public double getPercentUniqueValues(int[] array){
 
-    // Создайте метод, в который передайте заполненный выше массив и с помощью Set вычислите процент уникальных
-    // значений в данном массиве и верните его в виде числа с плавающей запятой.
-    // Для вычисления процента используйте формулу:
-    // процент уникальных чисел = количество уникальных чисел * 100 / общее количество чисел в массиве.
-    public double getPercentUniqueValues(int[] array){
-
-        return 0.0d;
-    }
+            Set<Integer> hset = new HashSet<>();
+            for (int item:array){
+                hset.add(item);
+            }
+            double result = hset.size() * 100 / array.length;
+            return result;
+        }
 }
--- src/main/java/ru/gb/jseminar/data/Notebook.java
+++ src/main/java/ru/gb/jseminar/data/Notebook.java
@@ -1,4 +1,103 @@
 package ru.gb.jseminar.data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Notebook {
+    private String model;
+    private String os;
+    private int ram;
+    private int hdd;
+    private String processor;
+    private double display;
+    private String color;
+    private int cost;
+
+    public Notebook(String model, String os, int ram, int hdd, String processor, double display, String color, int cost) {
+        this.model = model;
+        this.os = os;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.processor = processor;
+        this.display = display;
+        this.color = color;
+        this.cost = cost;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getOs() {
+        return os;
+    }
+    public void setOs(String os) {
+        this.os = os;
+    }
+    public int getRam() {
+        return ram;
+    }
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+    public int getHdd() {
+        return hdd;
+    }
+    public void setHd(int hdd) {
+        this.hdd = hdd;
+    }
+    public String getProcessor() {
+        return processor;
+    }
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+    public double getDisplay() {
+        return display;
+    }
+    public void setDisplay(double display) {
+        this.display = display;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public int getCost() {
+        return cost;
+    }
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+    public Map<String,Object> mapNotebook (){
+        Map<String,Object> mapNb = new HashMap<>();
+        mapNb.put("model",model);
+        mapNb.put("os",os);
+        mapNb.put("ram",ram);
+        mapNb.put("hdd",hdd);
+        mapNb.put("processor",processor);
+        mapNb.put("display",processor);
+        mapNb.put("color",color);
+        mapNb.put("cost",cost);
+
+        return mapNb;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "\nНоутбук{" +
+                "модель: " + model + ", " +
+                "ОС: " + os + ", " +
+                "память: " + ram + ", " +
+                "накопитель: " + hdd + ", " +
+                "процессор: " + processor + ", " +
+                "экран: " + display + ", " +
+                "цвет: " + color + ", " +
+                "цена: " + cost +
+                "}";
+    }
 }
