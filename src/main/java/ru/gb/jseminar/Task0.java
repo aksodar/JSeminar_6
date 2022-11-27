3152750a53ccd8ef2b386741c348d17ee3828f8d
dmitry kuznetsov <dmitriik06@bk.ru>
2022-11-27 12:09:40 +0300

    Homework 6 is presented

--- src/main/java/ru/gb/jseminar/Task0.java
+++ src/main/java/ru/gb/jseminar/Task0.java
@@ -1,26 +1,34 @@
 package ru.gb.jseminar;
 
+import java.util.*;
+
 public class Task0 {
 
     public static void main(String[] args) {
-
+        Task0 task0 = new Task0();
+        task0.initHashSet();
+        task0.initLinkedHashSet();
+        task0.initTreeSet();
     }
 
     // Создайте HashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
     // Распечатайте содержимое данного множества.
     public void initHashSet(){
-
+        Set<Integer> set = new HashSet<>(Arrays.asList(1, 2, 3, 2, 4, 5, 6, 3));
+        System.out.println(set);
     }
 
     // Создайте LinkedHashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
     // Распечатайте содержимое данного множества.
     public void initLinkedHashSet(){
-
+        LinkedHashSet<Integer> set = new LinkedHashSet<>(Arrays.asList(7, 2, 3, 2, 4, 5, 6, 3));
+        System.out.println(set);
     }
 
     // Создайте TreeSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
     // Распечатайте содержимое данного множества.
     public void initTreeSet(){
-
+        Set<Integer> set = new TreeSet<>(Arrays.asList(1, 2, 3, 2, 4, 5, 6, 3));
+        System.out.println(set);
     }
 }
--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,15 +1,28 @@
 package ru.gb.jseminar;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Task1 {
 
     public static void main(String[] args) {
-
+        Task1 task1 = new Task1();
+        int arSize = 100;
+        int[] result = task1.initArray(arSize);
+        System.out.println(Arrays.toString(result));
+        System.out.println(task1.getPercentUniqueValues(result));
     }
 
     // Напишите метод, который заполнит массив из 1000 элементов случайными цифрами от 0 до 24.
-    public int[] initArray(){
-
-        return new int[]{};
+    public int[] initArray(int arraysize){
+        Random random = new Random();
+        int[] myArray = new int[arraysize];
+        for (int i = 0; i < arraysize; i++) {
+            myArray[i] = random.nextInt(10);
+        }
+        return myArray;
     }
 
     // Создайте метод, в который передайте заполненный выше массив и с помощью Set вычислите процент уникальных
@@ -17,7 +30,10 @@ public class Task1 {
     // Для вычисления процента используйте формулу:
     // процент уникальных чисел = количество уникальных чисел * 100 / общее количество чисел в массиве.
     public double getPercentUniqueValues(int[] array){
-
-        return 0.0d;
+        Set<Integer> set = new HashSet<>();
+        for (int element: array) {
+            set.add(element);
+        }
+        return set.size() * 100 / array.length;
     }
 }
