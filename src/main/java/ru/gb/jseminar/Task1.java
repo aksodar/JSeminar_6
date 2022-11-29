648a7ba346cab335057d753002beb5ad21158e06
YuliyaStuchilova <yulia.s9314@gmail.com>
2022-11-29 14:59:09 +0300

    Homework

--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,23 +1,38 @@
 package ru.gb.jseminar;
 
+import java.util.*;
+
 public class Task1 {
 
     public static void main(String[] args) {
+        Task1 tk1 = new Task1();
+        int[] array = tk1.initArray(1000);
+        System.out.println(Arrays.toString(array));
+        System.out.println(tk1.getPercentUniqueValues(array));
 
     }
 
     // Напишите метод, который заполнит массив из 1000 элементов случайными цифрами от 0 до 24.
-    public int[] initArray(){
+    public int[] initArray(int arraysSize){
+        Random random = new Random();
+        int[] myArray = new int[arraysSize];
+        for (int i = 0; i < arraysSize; i++) {
+            myArray[i] = random.nextInt(10);
+        }
 
-        return new int[]{};
-    }
+        return myArray;}
 
     // Создайте метод, в который передайте заполненный выше массив и с помощью Set вычислите процент уникальных
     // значений в данном массиве и верните его в виде числа с плавающей запятой.
     // Для вычисления процента используйте формулу:
     // процент уникальных чисел = количество уникальных чисел * 100 / общее количество чисел в массиве.
     public double getPercentUniqueValues(int[] array){
+        Set<Integer> set = new HashSet<>();
+        for (int item: array) {
+            set.add(item);
+        }
+        double result = set.size()*100/array.length;
 
-        return 0.0d;
+        return result;
     }
 }
--- src/main/java/ru/gb/jseminar/data/Notebook.java
+++ src/main/java/ru/gb/jseminar/data/Notebook.java
@@ -1,4 +1,64 @@
 package ru.gb.jseminar.data;
 
 public class Notebook {
+    private String model;
+    private int ram;
+    private String color;
+    private String oS;
+    private int diagonal;
+
+    public Notebook(String model, int ram, String color, String oS, int diagonal) {
+        this.model = model;
+        this.ram = ram;
+        this.color = color;
+        this.oS = oS;
+        this.diagonal = diagonal;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getoS() {
+        return oS;
+    }
+
+    public void setoS(String oS) {
+        this.oS = oS;
+    }
+
+    public int getDiagonal() {
+        return diagonal;
+    }
+
+    public void setDiagonal(int diagonal) {
+        this.diagonal = diagonal;
+    }
+
+    public String get(String disgonal) {
+        return disgonal;
+    }
+    public String toString() {
+        return String.format("model:'%s' ram:%d color:'%s' oS:'%s' diagonal:%d", model, ram, color, oS, diagonal);
+    }
 }
