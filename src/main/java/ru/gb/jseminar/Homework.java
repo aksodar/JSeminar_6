ef8e64123c1a6e5df319f3c603d6e21caecd0e30
yana sarvarova <serg.yana@mail.ru>
2022-12-05 19:42:36 +0300

    Add homework

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,11 +1,12 @@
 package ru.gb.jseminar;
-
-import ru.gb.jseminar.data.Notebook;
-
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class Homework {
+
 
     // 1) Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
     // 2) Создать коллекцию ноутбуков.
@@ -14,17 +15,82 @@ public class Homework {
     //      Пример: ОЗУ - Значение, Объем ЖД - Значение, Операционная система - Значение, Цвет - Значение
     // 4) Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
     public static void main(String[] args) {
-
+        Homework homework = new Homework();
+        List <Notebook> notebooks = homework.initListNotebooks();
+        Map<String,String> params = new HashMap<>();
+        System.out.println(homework.filter(params, notebooks));
     }
 
     public List<Notebook> initListNotebooks(){
-
-        return null;
+        Notebook nb1 = new Notebook("Acer", 793, 225000, "black", "Windows", 256, 40);
+        Notebook nb2 = new Notebook("Asus", 882, 100000, "black", "Windows", 512,80);
+        Notebook nb3 = new Notebook("Samsung", 881, 150000, "black", "Windows", 256, 40);
+        Notebook nb4 = new Notebook("Apple", 100, 300000, "silver", "Mac OC", 512,80);
+        Notebook nb5 = new Notebook("Apple", 200, 225000, "red", "Mac OC", 256, 40);
+        Notebook nb6 = new Notebook("Acer", 882, 100000, "black", "Windows", 512,80);
+        List<Notebook> notebooks = new ArrayList<>();
+        notebooks.add(nb1);
+        notebooks.add(nb2);
+        notebooks.add(nb3);
+        notebooks.add(nb4);
+        notebooks.add(nb5);
+        notebooks.add(nb6);
+    return notebooks;
     }
 
     public List<Notebook> filter(Map<String, String> params, List<Notebook> notebooks){
-
-        return null;
+        params.put("ozu", "256");
+        params.put("valueZhD", "40");
+        params.put("operatingSystem", "Windows");
+        params.put("color", "black");
+        List<Notebook> filter = new ArrayList<>();
+        for (Notebook notebook : notebooks) {
+            int count = 0;
+            for (String key : params.keySet()) {
+                if (key.equals("name")) {
+                    if (params.get(key).equalsIgnoreCase(notebook.getName())) {
+                        count = count +1;
+                    }
+                }
+                if (Objects.equals(key, "model")) {
+                    if (params.get(key).equalsIgnoreCase(String.valueOf(notebook.getModel()))) {
+                        count = count + 1;
+                    }
+                }
+                if (key.equals("operatingSystem")) {
+                    if (params.get(key).equalsIgnoreCase(notebook.getOperatingSystem())) {
+                        count = count +1;
+                    }
+                }  
+                
+                if (key.equals("color")) {
+                    if (params.get(key).equalsIgnoreCase(notebook.getColor())) {
+                        count = count +1;
+                    }
+                } 
+                
+                if (Objects.equals(key, "ozu")) {
+                    if (params.get(key).equalsIgnoreCase(String.valueOf(notebook.getOzu()))) {
+                        count = count + 1;
+                    }
+                }
+                if (Objects.equals(key, "valueZhD")) {
+                    if (params.get(key).equalsIgnoreCase(String.valueOf(notebook.getValueZhD()))) {
+                        count = count + 1;
+                    }
+                }
+                if (Objects.equals(key, "price")) {
+                    if (params.get(key).equalsIgnoreCase(String.valueOf(notebook.getPrice()))) {
+                        count = count + 1;
+                    }
+                }
+                if (count == params.size()) {
+                    filter.add(notebook);
+                }  
+            }
+            
+        }
+        return filter;
     }
 
 }
--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,15 +1,28 @@
 package ru.gb.jseminar;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Task1 {
 
     public static void main(String[] args) {
-
+        Task1 task1 = new Task1();
+        int[] array = task1.initArray(1000);
+        System.out.println(Arrays.toString(array));
+        System.out.println(task1.getPercentUniqueValues(array));
     }
 
     // Напишите метод, который заполнит массив из 1000 элементов случайными цифрами от 0 до 24.
-    public int[] initArray(){
-
-        return new int[]{};
+    public int[] initArray(int arraySize){
+        Random random = new Random();
+        int[] myArray = new int [arraySize];
+        for ( int i = 0; i < myArray.length; i++) {
+            myArray[i] = random.nextInt(11);
+            
+        }
+        return myArray ;
     }
 
     // Создайте метод, в который передайте заполненный выше массив и с помощью Set вычислите процент уникальных
@@ -17,7 +30,12 @@ public class Task1 {
     // Для вычисления процента используйте формулу:
     // процент уникальных чисел = количество уникальных чисел * 100 / общее количество чисел в массиве.
     public double getPercentUniqueValues(int[] array){
-
-        return 0.0d;
+        double result;
+        Set<Integer> set = new HashSet<>();
+        for (Integer integer : array) {
+            set.add(integer);
+        }
+        result = set.size()*100/(double)array.length;
+        return result;
     }
 }
