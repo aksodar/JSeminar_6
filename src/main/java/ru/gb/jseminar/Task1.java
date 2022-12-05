09148536268e0d6e1e17e1bab082f7ddf030ca94
Fray <kozar20000@mail.ru>
2022-12-05 18:54:50 +0300

    Notebook_Store

--- src/main/java/ru/gb/jseminar/Task1.java
+++ src/main/java/ru/gb/jseminar/Task1.java
@@ -1,15 +1,30 @@
 package ru.gb.jseminar;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.logging.Logger;
+
 public class Task1 {
 
     public static void main(String[] args) {
-
+        Task1 tk = new Task1();
+        Logger lg = Logger.getLogger(Task1.class.getName());
+        int[] array = tk.initArray(1000);
+        lg.info(Arrays.toString(array));
+        //lg.info(Arrays.toString(tk.getPercentUniqueValues(array)));
+        System.out.println(tk.getPercentUniqueValues(array));
     }
 
     // Напишите метод, который заполнит массив из 1000 элементов случайными цифрами от 0 до 24.
-    public int[] initArray(){
-
-        return new int[]{};
+    public int[] initArray(int arraySize){
+        Random random = new Random();
+        int[] myArray = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            myArray[i] = random.nextInt(10);
+        }
+        return myArray;
     }
 
     // Создайте метод, в который передайте заполненный выше массив и с помощью Set вычислите процент уникальных
@@ -17,7 +32,11 @@ public class Task1 {
     // Для вычисления процента используйте формулу:
     // процент уникальных чисел = количество уникальных чисел * 100 / общее количество чисел в массиве.
     public double getPercentUniqueValues(int[] array){
-
-        return 0.0d;
+        Set<Integer> mySet = new HashSet<>();
+        for(int item: array){
+            mySet.add(item);
+        }
+        double result = mySet.size() * 100/ (double)array.length;
+        return result;
     }
 }
--- src/test/java/ru/gb/jseminar/HomeworkTest.java
+++ src/test/java/ru/gb/jseminar/HomeworkTest.java
@@ -1,5 +1,33 @@
 package ru.gb.jseminar;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import ru.gb.jseminar.data.Notebook;
+
 class HomeworkTest {
 
+    @Test
+    void filterTest(){
+       List<Notebook> notebookTest = new ArrayList<>();
+       Notebook n1 = new Notebook("Asus", 12, "Linux", "blue", 14);
+       Notebook n2 = new Notebook("DELL", 16, "Windows", "red", 16);
+       notebookTest.add(n1);
+       notebookTest.add(n2);
+
+       Map<String, String> mapFilterTest = new HashMap<>();
+       mapFilterTest.put("color", "blue");
+
+       Homework homework = new Homework();
+       List<Notebook> actual = homework.filter(mapFilterTest, notebookTest);
+       List<Notebook> expected = new ArrayList<>();
+       expected.add(n1);
+
+       Assertions.assertIterableEquals(expected, actual);
+    }
 }
