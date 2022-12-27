e26c313df4555e2b643538edec42305b5fa74df3
MikhailNagornykh <stumpik@gmail.com>
2022-12-27 23:52:30 +0300

    Finish_Java_Lesson

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -4,6 +4,9 @@ import ru.gb.jseminar.data.Notebook;
 
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Logger;
 
 public class Homework {
 
@@ -15,16 +18,62 @@ public class Homework {
     // 4) Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
     public static void main(String[] args) {
 
+        Homework homework = new Homework();
+
+        Map<String, String> filterMap = new HashMap<>();
+        filterMap.put("RAM", "16");
+        filterMap.put("SSD", "1024");
+        filterMap.put("size", "15");
+        filterMap.put("color", "black");
+
+        List<Notebook> resultList = homework.filter(filterMap, homework.initListNotebooks());
+        for (Notebook result : resultList) {
+            
+            System.out.println(result);
+        }
+        
+
+        
     }
+
 
     public List<Notebook> initListNotebooks(){
 
-        return null;
+        List<Notebook> notebooks = new ArrayList<>();
+        
+        notebooks.add(new Notebook(80000 , "Acer", 8, 512, "Linux", 15, "black"));
+        notebooks.add(new Notebook(85000 , "Asus", 16, 512, "Windows", 15, "red"));
+        notebooks.add(new Notebook(100000 , "Lenovo", 16, 1024, "None", 15, "black"));
+        notebooks.add(new Notebook(98000 , "MSI", 16, 1024, "Linux", 14, "white"));
+        notebooks.add(new Notebook(106000 , "HP", 16, 1024, "Windows", 15, "black"));
+        return notebooks;
+
     }
 
     public List<Notebook> filter(Map<String, String> params, List<Notebook> notebooks){
 
-        return null;
-    }
+        List<Notebook> nbFiltr = new ArrayList<>();
+        
+        for (Notebook notebook : notebooks) {
+            Map<String, String> nbParam = notebook.getAllParams();
+            boolean flag = true;
+            for (String param : params.keySet()) {
+                if (nbParam.containsKey(param)) {
+                    if (!params.get(param).equals(nbParam.get(param))) {
+                        flag = false;
+                        break;
+                    }
+                } else {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                nbFiltr.add(notebook);
+            }
+        }
 
+        return nbFiltr;
+    }
+    
 }
--- src/main/java/ru/gb/jseminar/data/Notebook.java
+++ src/main/java/ru/gb/jseminar/data/Notebook.java
@@ -1,4 +1,110 @@
 package ru.gb.jseminar.data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Notebook {
-}
+
+    private int price;
+    private String manufacturer;
+    private int RAM;
+    private int SSD;
+    private String OS;
+    private int size;
+    private String color;
+    
+
+    public Notebook (int price, String manufacturer, int ram, int ssd, String os, int size, String color) {
+        
+        this.price = price;
+        this.manufacturer = manufacturer;
+        this.RAM = ram;
+        this.SSD = ssd;
+        this.OS = os;
+        this.size = size;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Price:%d rub Manufacturer:'%s' RAM:%dGB SSD:%dGB OS:'%s' Size:%d' Color:'%s'",price, manufacturer, RAM, SSD, OS, size, color);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        var nb = (Notebook) obj;
+        return price == nb.price && manufacturer.equals(nb.manufacturer) && RAM == nb.RAM && SSD == nb.SSD && OS.equals(nb.OS) && size == nb.size && color.equals(nb.color);
+    }
+
+    public Map<String, String> getAllParams () {
+        Map<String, String> params = new HashMap<>();
+
+        params.put("price", String.valueOf(price));
+        params.put("manufacturer", manufacturer);
+        params.put("RAM", String.valueOf(RAM));
+        params.put("SSD", String.valueOf(SSD));
+        params.put("OS", OS);
+        params.put("size", String.valueOf(size));
+        params.put("color", color);
+        
+        return params;
+    }
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getRAM() {
+        return RAM;
+    }
+
+    public void setRAM(int RAM) {
+        this.RAM = RAM;
+    }
+
+    public int getSSD() {
+        return SSD;
+    }
+
+    public void setSSD(int SSD) {
+        this.SSD = SSD;
+    }
+
+    public String getOS() {
+        return OS;
+    }
+
+    public void setOS(String OS) {
+        this.OS = OS;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}    
