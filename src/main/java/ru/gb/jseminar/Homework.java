eae4d3903618af3def94aa2d78a8306baa556ed8
azolanda <uboikov@yandex.ru>
2023-01-12 17:38:23 +0300

    Task Notebook added (first commit)

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -2,29 +2,67 @@ package ru.gb.jseminar;
 
 import ru.gb.jseminar.data.Notebook;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Homework {
 
-    // 1) Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
+    // 1) Подумать над структурой класса Ноутбук для магазина техники - выделить
+    // поля и методы. Реализовать в java.
     // 2) Создать коллекцию ноутбуков.
-    // 3) Написать мапу, которая будет содержать критерий (или критерии) фильтрации и выведет
-    //      ноутбуки, отвечающие фильтру.
-    //      Пример: ОЗУ - Значение, Объем ЖД - Значение, Операционная система - Значение, Цвет - Значение
-    // 4) Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
-    public static void main(String[] args) {
+    // 3) Написать мапу, которая будет содержать критерий (или критерии) фильтрации
+    // и выведет
+    // ноутбуки, отвечающие фильтру.
+    // Пример: ОЗУ - Значение, Объем ЖД - Значение, Операционная система - Значение,
+    // Цвет - Значение
+    // 4) Отфильтровать ноутбуки их первоначального множества и вывести проходящие
+    // по условиям.
 
+    public static void main(String[] args) throws Exception {
+        Homework hw = new Homework();
+        List<Notebook> notebookList = hw.initListNotebooks();
+        Map<String, String> filter1 = new HashMap<>();
+        filter1.put("color", "silver");
+        System.out.println(hw.filter(filter1, notebookList));
     }
 
-    public List<Notebook> initListNotebooks(){
-
-        return null;
+    public List<Notebook> initListNotebooks() {
+        List<Notebook> noteList = new ArrayList<>();
+        noteList.add(new Notebook("Intel Core i5", "black", 15.6, 4, 500, 56944.99));
+        noteList.add(new Notebook("Intel Core i7", "silver", 15.4, 4, 500, 92999.99));
+        noteList.add(new Notebook("Intel Core i7", "pink", 17.3, 4, 500, 49999.99));
+        noteList.add(new Notebook("Intel Core i5", "silver", 17.0, 4, 500, 97599.99));
+        noteList.add(new Notebook("Intel Core i7", "black", 15.6, 16, 512, 79645.99));
+        noteList.add(new Notebook("Intel Core i7", "silver", 14.0, 8, 256, 39700.00));
+        noteList.add(new Notebook("AMD A9", "black", 11.6, 4, 64, 15399.99));
+        noteList.add(new Notebook("Intel Core i3", "black", 14.0, 4, 128, 18500.00));
+        noteList.add(new Notebook("Intel Core i3", "black", 13.4, 4, 256, 25000.00));
+        return noteList;
     }
 
-    public List<Notebook> filter(Map<String, String> params, List<Notebook> notebooks){
+    public List<Notebook> filter(Map<String, String> params, List<Notebook> notebooks) throws Exception {
+        if (notebooks == null || params == null) {
+            throw new Exception("Входные данные пустые");
+        }
+        List<Notebook> filteredList = new ArrayList<>();
+        for (Map.Entry<String, String> element : params.entrySet()) {
+            String key = element.getKey();
+            String methodName = "get" + key.substring(0, 1).toUpperCase() + key.toLowerCase().substring(1);
+            String value = element.getValue();
 
-        return null;
+            for (Notebook note : notebooks) {
+                // получаем метод по его названию-строке
+                Method method = note.getClass().getMethod(methodName, null);
+                if (method.invoke(note, null).equals(value)) {
+                    filteredList.add(note);
+                }
+            }
+        }
+
+        return filteredList;
     }
 
 }
--- src/main/java/ru/gb/jseminar/data/Notebook.java
+++ src/main/java/ru/gb/jseminar/data/Notebook.java
@@ -1,4 +1,92 @@
 package ru.gb.jseminar.data;
 
 public class Notebook {
+    private String processor;
+    private String color;
+    private Double screen;
+    private Integer memory;
+    private Integer hardDrive;
+    private Double price;
+    private Integer quantity;
+
+    public Notebook() {
+        processor = null;
+        color = null;
+        screen = null;
+        memory = null;
+        hardDrive = null;
+        price = null;
+        quantity = 0;
+    }
+
+    public Notebook(String processor, String color, Double screen, Integer memory, Integer hardDrive, Double price) {
+        this.processor = processor;
+        this.color = color;
+        this.screen = screen;
+        this.memory = memory;
+        this.hardDrive = hardDrive;
+        this.price = price;
+        quantity = 0;
+    }
+
+    public void setQuantity(Integer quantity) {
+        if (quantity > 0) {
+            this.quantity = quantity;
+        }
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Double getScreen() {
+        return screen;
+    }
+
+    public Integer getMemory() {
+        return memory;
+    }
+
+    public Integer getHardDrive() {
+        return hardDrive;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void decreaseQuantity(Integer decrease) {
+        if (decrease > 0 && this.quantity - decrease >= 0) {
+            this.quantity = this.quantity - decrease;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "\n--------------------->\n" + processor + "\n" + color + "\n" + screen + "\"\n" + memory + "GB\n"
+                + hardDrive + "GB\n"
+                + price
+                + "rub";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Notebook objToCompare = (Notebook) obj;
+        return this.processor.equals(objToCompare.processor) && this.color.equals(objToCompare.color)
+                && this.screen.equals(objToCompare.screen) && this.memory.equals(objToCompare.memory)
+                && this.hardDrive.equals(objToCompare.hardDrive) && this.price.equals(objToCompare.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return price.intValue() * screen.intValue() * memory * hardDrive * color.length() * processor.length();
+    }
 }
