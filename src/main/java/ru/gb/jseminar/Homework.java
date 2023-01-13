3ed7631c1534baac14f0852ca9c4a7b9c498d705
LutovinovKN <VesperLKN@mail.ru>
2023-01-13 16:19:40 +0300

    выполнено

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -1,30 +1,164 @@
 package ru.gb.jseminar;
 
+import ru.gb.jseminar.data.ANSIConstants;
 import ru.gb.jseminar.data.Notebook;
-
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Homework {
-
-    // 1) Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
-    // 2) Создать коллекцию ноутбуков.
-    // 3) Написать мапу, которая будет содержать критерий (или критерии) фильтрации и выведет
-    //      ноутбуки, отвечающие фильтру.
-    //      Пример: ОЗУ - Значение, Объем ЖД - Значение, Операционная система - Значение, Цвет - Значение
-    // 4) Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
     public static void main(String[] args) {
-
+        Homework nb = new Homework();
+        Set laptops = nb.getFullSetOfLaptops();
+        Set foundLaptops = nb.displayMenu(laptops);
+        if (foundLaptops.size() == 1) {
+            nb.printResult(foundLaptops);
+            System.out.println("-----------------------\n"+ANSIConstants.ANSI_GREEN + "Done" + ANSIConstants.ANSI_RESET);
+            System.exit(0);
+        }
+        if (foundLaptops.isEmpty()) {
+            System.out.println(ANSIConstants.ANSI_RED + "Laptops for desired parameters not found or incorrect choice! Please try again! " + ANSIConstants.ANSI_RESET);
+        }
+        else {
+            nb.printResult(foundLaptops);
+            while (!foundLaptops.isEmpty() || foundLaptops.size() > 1 ) {
+                foundLaptops = nb.deepSearch (foundLaptops);
+                if (foundLaptops.isEmpty()) {
+                    System.out.println(ANSIConstants.ANSI_RED + "Laptops for desired parameters not found or incorrect choice! Please try again! " + ANSIConstants.ANSI_RESET);
+                    System.exit(0);
+                }
+                nb.printResult(foundLaptops);
+                if (foundLaptops.size() == 1) {
+                    System.out.println("-----------------------\n"+ANSIConstants.ANSI_GREEN + "Done" + ANSIConstants.ANSI_RESET);
+                    System.exit(0);
+                }
+            }
+        }
+    }
+    public Set getFullSetOfLaptops() {
+        Notebook l1 = new Notebook(1, "Apple MacBook Pro", (float)16.2, (float)64.0, (float)512, "grey cosmic", (float)360000.0);
+        Notebook l2 = new Notebook(2, "MSI CreatorPro Z17", (float)17.0, (float)64.0, (float)256, "silver", (float)99000.0);
+        Notebook l3 = new Notebook(3, "GIGABYTE Aero 16", (float)16.0, (float)32.0, (float)512, "grey", (float)55000.0);
+        Notebook l4 = new Notebook(4, "MSI Vector GP66", (float)15.6, (float)16.0, (float)256, "black", (float)47000.0);
+        Notebook l5 = new Notebook(5, "Huawei MateBook D 16", (float)16.0, (float)16.0, (float)512, "pink", (float)77000.0);
+        Notebook l6 = new Notebook(6, "TECNO MegaBook T1", (float)15.6, (float)16.0, (float)512, "grey", (float)39000.0);
+        Notebook l7 = new Notebook(7, "Lenovo V15 G2 ITL", (float)15.6, (float)8.0, (float)256, "black", (float)44000.0);
+        Notebook l8 = new Notebook(8, "ASUS Vivobook 15", (float)17.5, (float)8.0, (float)512, "silver", (float)50000.0);
+        Set laps = new LinkedHashSet<>(Arrays.asList(l1,l2,l3,l4,l5,l6,l7,l8));
+        return laps;
+    }
+    public Set displayMenu(Set<Notebook> notebooks) {
+        Map parameters = new HashMap<>();
+        Scanner in = new Scanner(System.in);
+        System.out.println("\n"+ANSIConstants.ANSI_WHITE_BACKGROUND+ANSIConstants.ANSI_BLACK+"LAPTOPS SEARCH MODE"+ANSIConstants.ANSI_RESET);
+        System.out.println("----------------------------------------");
+        System.out.println("Choose option/options for further search: ");
+        System.out.println("----------------------------------------");
+        System.out.println("1.Display size");
+        System.out.println("2.CPU type");
+        System.out.println("3.SSD type");
+        System.out.println("4.Color");
+        System.out.println("5.Price");
+        System.out.println("6.See full list of laptops available");
+        System.out.println("7.Exit");
+        System.out.println("-------------------------------------");
+        System.out.print("Provide your choice here: ");
+        int choice = in.nextInt();
+        in.nextLine();
+        System.out.println("-------------------------------------");
+        Set findSet = new LinkedHashSet<>();
+        switch (choice) {
+            case 1:
+                System.out.print("Display size from: ");
+                Float d = in.nextFloat();
+                parameters.put("displaySize", d);
+                for (Notebook item : notebooks) {
+                    if ((float)parameters.get("displaySize") <= item.getDisplaySize()) {
+                        findSet.add(item);
+                    }
+                }
+                break;
+            case 2:
+                System.out.print("CPU capacity from: ");
+                Float c = in.nextFloat();
+                parameters.put("cpuType", c);
+                for (Notebook item : notebooks) {
+                    if ((float)parameters.get("cpuType") <= item.getCpuType()) {
+                        findSet.add(item);
+                    }
+                }
+                break;
+            case 3:
+                System.out.print("SSD capacity from: ");
+                Float s = in.nextFloat();
+                parameters.put("ssdSize", s);
+                for (Notebook item : notebooks) {
+                    if ((float)parameters.get("ssdSize") <= item.getSSDSize()) {
+                        findSet.add(item);
+                    }
+                }
+                break;
+            case 4:
+                System.out.print("Color desired: ");
+                String cl = in.nextLine();
+                parameters.put("color", cl);
+                for (Notebook item : notebooks) {
+                    if (parameters.containsValue(item.getColor())) {
+                        findSet.add(item);
+                    }
+                }
+                break;
+            case 5:
+                System.out.print("Price upto: ");
+                Float p = in.nextFloat();
+                parameters.put("price", p);
+                for (Notebook item : notebooks) {
+                    if ((float)parameters.get("price") >= item.getPrice()) {
+                        findSet.add(item);
+                    }
+                }
+                break;
+            case 6:
+                findSet = getFullSetOfLaptops();
+                break;
+            case 7:
+                System.out.println(ANSIConstants.ANSI_GREEN+"Search process finished"+ANSIConstants.ANSI_RESET);
+                System.exit(0);
+        }
+        return findSet;
+    }
+    public Set additionalDisplayMenu(Set<Notebook> notebooks) {
+        Set findSet1 = new LinkedHashSet<>();
+        System.out.println("-------------------------------------");
+        System.out.println(ANSIConstants.ANSI_YELLOW+"Continue filtering current list? "+ANSIConstants.ANSI_RESET);
+        Scanner in = new Scanner(System.in);
+        System.out.println("-------------------------------------");
+        System.out.println("1.Yes");
+        System.out.println("2.No");
+        System.out.print("Provide your choice here: ");
+        int choice = in.nextInt();
+        in.nextLine();
+        switch (choice) {
+            case 1:
+                findSet1 = displayMenu(notebooks);
+                break;
+            case 2:
+                System.out.println(ANSIConstants.ANSI_GREEN+"Search process finished"+ANSIConstants.ANSI_RESET);
+                System.exit(0);
+        }
+        return findSet1;
     }
 
-    public List<Notebook> initListNotebooks(){
-
-        return null;
+    public Set deepSearch (Set<Notebook> notebooks) {
+        Set newSet = new LinkedHashSet();
+        if (notebooks.size() > 1) {
+            newSet = additionalDisplayMenu(notebooks);
+        }
+        return newSet;
     }
 
-    public List<Notebook> filter(Map<String, String> params, List<Notebook> notebooks){
-
-        return null;
+    public void printResult(Set<Notebook> notebooks) {
+        System.out.println("====================================");
+        System.out.println(ANSIConstants.ANSI_WHITE_BACKGROUND+ANSIConstants.ANSI_BLACK+"List of laptops found: "+ANSIConstants.ANSI_RESET);
+        System.out.println(notebooks);
+        System.out.println("");
     }
-
 }
