06055959dee280b63cd92b1ad3270158ad9ad2e8
Serg-Geek <svgordienko@yandex.ru>
2023-01-13 20:50:58 +0400

    add Homework6

--- src/main/java/ru/gb/jseminar/Homework.java
+++ src/main/java/ru/gb/jseminar/Homework.java
@@ -2,8 +2,7 @@ package ru.gb.jseminar;
 
 import ru.gb.jseminar.data.Notebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Homework {
 
@@ -14,17 +13,116 @@ public class Homework {
     //      Пример: ОЗУ - Значение, Объем ЖД - Значение, Операционная система - Значение, Цвет - Значение
     // 4) Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
     public static void main(String[] args) {
+        Homework hw = new Homework();
+        System.out.println(hw.initListNotebooks());
+        ArrayList hw1 = new ArrayList(hw.initListNotebooks());
 
+        System.out.println("Результат фильтра \n" + hw.filter(searchFiltre(), hw1).toString());
     }
 
-    public List<Notebook> initListNotebooks(){
+    public ArrayList<String> initListNotebooks() {
+        Notebook Notebook1 = new Notebook("asus", "Zenbook Pro 14 OLED", 2000, 16, "Windows 11", "grey",
+                75000);
+        Notebook Notebook2 = new Notebook("lenovo", "V 14", 1000, 16, "Windows 11", "grey",
+                50000);
+        Notebook Notebook3 = new Notebook("dell", "240 G7 HP Laptop", 256, 8, "Windows 11", "Black",
+                40000);
+        Notebook Notebook4 = new Notebook("apple", "MacBook Air", 2000, 16, "MacOS", "white",
+                110000);
+        ArrayList listNotebook = new ArrayList();
+        listNotebook.add(Notebook1);
+        listNotebook.add(Notebook2);
+        listNotebook.add(Notebook3);
+        listNotebook.add(Notebook4);
 
-        return null;
+        return listNotebook;
     }
 
-    public List<Notebook> filter(Map<String, String> params, List<Notebook> notebooks){
-
-        return null;
+    public List<Notebook> filter(Map<String, String> params, List<Notebook> notebooks) throws IllegalStateException {
+        ArrayList listfilter = new ArrayList();
+        for (Notebook item : notebooks) {
+            int count = 0;
+            for (String param : params.keySet()) {
+                if (item.paramNotebook().containsValue(params.get(param))) {
+                    count++;
+                }
+            }
+            if (count == params.keySet().size()) {
+                listfilter.add(item);
+            }
+        }
+        if (!listfilter.isEmpty()) {
+            return listfilter;
+        }
+        throw new IllegalStateException("No notebook");
     }
 
+    public static Map<String, String> searchFiltre() {
+        Map<String, String> searchKeys = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        String keyfield = "";
+        String val = "";
+        System.out.println("input num for filter: \n"
+                + "1  RAM\n"
+                + "2 SSD\n"
+                + "3 OS\n"
+                + "4 COLOR\n");
+        int field = sc.nextInt();
+        if (field == 1) {
+            keyfield = "ramSize";
+            System.out.println("input num value: \n"
+                    + "1 - 16Gb \n"
+                    + "2 - 8Gb");
+            int value = sc.nextInt();
+            if (value == 1) {
+                val = "16";
+            } else {
+                val = "8";
+            }
+
+        }
+        if (field == 2) {
+            keyfield = "ssdSize";
+            System.out.println("input num value: \n"
+                    + "1 - 2000Gb \n"
+                    + "2 - 1000Gb\n" +
+                    "3 - 256Gb");
+            int value = sc.nextInt();
+            if (value == 1) {
+                val = "2000";
+            } else if (value == 2) {
+                val = "1000";
+            } else {
+                val = "256";
+            }
+        }
+        if (field == 3) {
+            keyfield = "os";
+            System.out.println("input num value: \n"
+                    + "1 - W \n"
+                    + "2 - M");
+            int value = sc.nextInt();
+            if (value == 1) {
+                val = "Windows 11";
+            } else {
+                val = "MacOS";
+            }
+        }
+        if (field == 4) {
+            keyfield = "color";
+            System.out.println("input num value: \n"
+                    + "1 - grey \n"
+                    + "2 - black");
+            int value = sc.nextInt();
+            if (value == 1) {
+                val = "grey";
+            } else {
+                val = "Black";
+            }
+        }
+        searchKeys.put(keyfield, val);
+        return searchKeys;
+    }
 }
+
+
--- src/main/java/ru/gb/jseminar/data/Notebook.java
+++ src/main/java/ru/gb/jseminar/data/Notebook.java
@@ -1,4 +1,53 @@
 package ru.gb.jseminar.data;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Notebook {
+
+
+     String brand;
+     String model;
+     Integer ssdSize;
+     Integer ramSize;
+     String os;
+     String color;
+     Integer price;
+
+    @Override
+    public String toString() {
+        String str;
+        str = "============================\n" + brand + " " + model + "\n" + ssdSize + "Gb\n" + ramSize + "Gb\n" +
+                os + "\n" + color + "\n" + price + "rub" + "\n";
+        return str;
+    }
+
+    public Notebook(String brand, String model, Integer ssdSize, Integer ramSize, String os, String color, Integer price) {
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+        this.os = os;
+        this.ramSize = ramSize;
+        this.ssdSize = ssdSize;
+        this.model = model;
+    }
+
+    public  Map<String, Object> paramNotebook() {
+//        Scanner scanner = new Scanner(System.in);
+        Map<String, Object> param = new HashMap<>();
+        param.put("brand", brand);
+        param.put("model", model);
+        param.put("os", os);
+
+        param.put("ram", ramSize.toString());
+        param.put("ssd", ssdSize.toString());
+        param.put("color", color);
+        param.put("price", price.toString());
+        Scanner scanner = new Scanner(System.in);
+        return param;
+    }
 }
+
+
+
