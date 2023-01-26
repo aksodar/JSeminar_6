f9f0d5cf859b55800bd4dc406c33d9b4a80b1a52
karmusha <karmusha@gmail.com>
2023-01-26 12:37:54 +0100

    added sem1

---
+++ src/main/java/ru/gb/jseminar/sem1/BottleMachine.java
@@ -0,0 +1,11 @@
+package ru.gb.jseminar.sem1;
+
+import java.util.List;
+
+public class BottleMachine extends VendingMachine{
+
+    public BottleMachine(List<Product> prodList){
+        super(prodList);
+    }
+    
+}
---
+++ src/main/java/ru/gb/jseminar/sem1/Product.java
@@ -0,0 +1,35 @@
+package ru.gb.jseminar.sem1;
+
+public class Product {
+    private String name;
+    private Double cost;
+
+    public Product(String name, Double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+    
+    public Double getCost() {
+        return this.cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [name = " + name + ", cost = " + cost + "]";
+    }
+
+    
+}
+
